504c336e7a10099a7b333336c6fa27bc074fedc3
dagai <602931725@qq.com>
2019-12-07 16:33:21 +0800

    FloodProblem Init is ready

--- includes/FloodProblem.h
+++ includes/FloodProblem.h
@@ -35,72 +35,245 @@
  */
 #ifndef FUTURE_STREAM_FLOOD_PROMBLEM_H
 #define FUTURE_STREAM_FLOOD_PROMBLEM_H
+
 #include <utility>
-template<class Type>
+#include <limits>
+
+template <class Type>
 class TwoDimensinalObj;
 
-template<class Type,const size_t xLen, const size_t yLen>
+template <class Type, const size_t xLen, const size_t yLen>
 class TwoDimensinalObj<Type[xLen][yLen]>
 {
 public:
+    enum
+    {
+        XLEN = xLen,
+        YLEN = yLen
+    };
     typedef Type DataContinerType[xLen][yLen];
-    TwoDimensinalObj(DataContinerType & data):_dataContiner(data)
-    {}
+    TwoDimensinalObj(DataContinerType &data) : _dataContiner(data)
+    {
+    }
 
-    inline const Type& GetValue(const size_t x,const size_t y) {return _dataContiner[x][y];}
-    inline void SetValue(const size_t x,const size_t y,const Type &value){_dataContiner[x][y] = value;}
+    inline const Type &GetValue(const size_t x, const size_t y) { return _dataContiner[x][y]; }
+    inline void SetValue(const size_t x, const size_t y, const Type &value) { _dataContiner[x][y] = value; }
 
-    inline DataContinerType& Cells() {return _dataContiner;}
-    inline const size_t GetXLen(){return xLen;}
-    inline const size_t GetYLen(){return yLen;}
+    inline DataContinerType &Cells() { return _dataContiner; }
+    inline const size_t GetXLen() { return xLen; }
+    inline const size_t GetYLen() { return yLen; }
+
 private:
     DataContinerType &_dataContiner;
 };
 
-template<class Type>
-TwoDimensinalObj<Type>  MakeTwoDimensinalObj(Type &data)
+template <class Type>
+TwoDimensinalObj<Type> MakeTwoDimensinalObj(Type &data)
 {
-    return TwoDimensinalObj<Type>(data); 
+    return TwoDimensinalObj<Type>(data);
 }
+
+class VertexObj
+{
+public:
+    typedef size_t PrecursorType;
+    typedef size_t WeightType;
+    enum
+    {
+        NIL = std::numeric_limits<size_t>::max()
+    };
+
+private:
+    struct _Vertex
+    {
+        _Vertex() : pre(NIL), path(NIL), weight(NIL) {}
+
+        PrecursorType pre;
+        WeightType path;
+        WeightType weight;
+    };
+
+public:
+    typedef _Vertex VertexType;
+
+    VertexObj(size_t len)
+    {
+        _vertexObjs.resize(len);
+    }
+
+    inline const PrecursorType &GetPrecursor(size_t index)
+    {
+        return _vertexObjs[index].pre;
+    }
+
+    inline void SetPrecursor(size_t index, const PrecursorType &value)
+    {
+        _vertexObjs[index].pre = value;
+    }
+
+    inline const WeightType &GetPathValue(size_t index)
+    {
+        return _vertexObjs[index].path;
+    }
+
+    inline void SetPathValue(size_t index, const WeightType &value)
+    {
+        _vertexObjs[index].path = value;
+    }
+
+    inline const WeightType &GetWeight(size_t index)
+    {
+        return _vertexObjs[index].weight;
+    }
+
+    inline void SetWeight(size_t index, const WeightType &value)
+    {
+        _vertexObjs[index].weight = value;
+    }
+
+private:
+    std::vector<_Vertex> _vertexObjs;
+};
 
 class AnchorObj
 {
+public:
+    enum AnchorIndex
+    {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT,
+        ANCHOR_LENGTH
+    };
+    typedef std::pair<size_t, size_t> AnchorType;
 
+    inline void Insert(AnchorIndex index, size_t x, size_t y)
+    {
+        _anchorObjs[index] = std::make_pair(x, y);
+    }
+    inline const AnchorType &GetAnchor(const AnchorIndex ind)
+    {
+        return _anchorObjs[ind];
+    }
+
+private:
+    AnchorType _anchorObjs[ANCHOR_LENGTH];
 };
 
-
-template<class Type>
-class FloodProblem{
+template <class Type>
+class FloodProblem
+{
 public:
-    FloodProblem(Type &data):_cells(data){}
-    
-    bool GetAnchorObj(AnchorObj &anchor){
-        const size_t xLen = _cells.GetXLen();
-        const size_t yLen = _cells.GetYLen();
-        for(size_t i = 0; i < xLen; ++i)
+    FloodProblem(Type &data) : _cells(data), _vertexObj(TwoDimensinalObj<Type>::XLEN * TwoDimensinalObj<Type>::YLEN) {}
+
+    void InitSource()
+    {
+        size_t anchorIndex = AnchorObj::TOP_LEFT;
+        size_t vertexIndex = 0;
+
+        size_t max_left = 0;
+        size_t max_top = 0;
+
+        const size_t xLen = _cells.XLEN;
+        const size_t yLen = _cells.YLEN;
+
+        for (size_t i = 0; i < xLen; ++i)
         {
-            for(size_t j = 0; j < yLen; ++ j)
-                if(_cells.Cells()[i][j] == -1)
+            for (size_t j = 0; j < yLen; ++j)
+            {
+                if (_cells.Cells()[i][j] == 0)
                 {
-                    /**
-                     * @brief handle anchors
-                     * 
-                     */
+                    if (anchorIndex == AnchorObj::TOP_LEFT)
+                    {
+                        max_left = i;
+                        max_top = j;
+
+                        ++anchorIndex;
+                    }
+                    else if (anchorIndex == AnchorObj::TOP_RIGHT)
+                    {
+                        if (max_top <= j)
+                        {
+                            _anchor.Insert(AnchorObj::TOP_LEFT, max_left, max_top);
+                            _anchor.Insert(AnchorObj::TOP_RIGHT, i, j);
+                        }
+                        else
+                        {
+                            _anchor.Insert(AnchorObj::TOP_LEFT, i, j);
+                            _anchor.Insert(AnchorObj::TOP_RIGHT, max_left, max_top);
+                        }
+                        ++anchorIndex;
+                    }
+                    else if (anchorIndex == AnchorObj::BOTTOM_LEFT)
+                    {
+                        max_left = i;
+                        max_top = j;
+
+                        ++anchorIndex;
+                    }
+                    else if (anchorIndex == AnchorObj::BOTTOM_RIGHT)
+                    {
+                        if (max_top <= j)
+                        {
+                            _anchor.Insert(AnchorObj::BOTTOM_LEFT, max_left, max_top);
+                            _anchor.Insert(AnchorObj::BOTTOM_RIGHT, i, j);
+                        }
+                        else
+                        {
+                            _anchor.Insert(AnchorObj::BOTTOM_LEFT, i, j);
+                            _anchor.Insert(AnchorObj::BOTTOM_RIGHT, max_left, max_top);
+                        }
+
+                        ++anchorIndex;
+                    }
+                    else
+                    {
+                        assert(0);
+                    }
                 }
+                
+                _vertexObj.SetWeight(vertexIndex, _cells.Cells()[i][j]);
+                ++ vertexIndex;
+            }
         }
-        return false;
+    }
+
+    inline void Relax(size_t u, size_t v, size_t wight)
+    {
+        //if(GetVertexObj(v).dest > GetVertexObj(v) + )
+    }
+
+    inline const AnchorObj::AnchorType &GetAnchorObj(AnchorObj::AnchorIndex index)
+    {
+        return _anchor.GetAnchor(index);
+    }
+
+    inline const VertexObj::PrecursorType &GetPrecursor(size_t index)
+    {
+        return _vertexObj.GetPrecursor(index);
+    }
+
+    inline const VertexObj::WeightType &GetPathValue(size_t index)
+    {
+        return _vertexObj.GetPathValue(index);
+    }
+
+    inline const VertexObj::WeightType &GetWeight(size_t index)
+    {
+        return _vertexObj.GetWeight(index);
     }
 
 private:
     TwoDimensinalObj<Type> _cells;
+    VertexObj _vertexObj;
+    AnchorObj _anchor;
 };
 
-
-
-template<class Type>
-FloodProblem<Type>  MakeFloodProblemInstance(Type &data)
+template <class Type>
+FloodProblem<Type> MakeFloodProblemInstance(Type &data)
 {
-    return FloodProblem<Type>(data); 
+    return FloodProblem<Type>(data);
 }
 
 #endif
--- tests/FloodProblemTest.cpp
+++ tests/FloodProblemTest.cpp
@@ -4,7 +4,7 @@
  * Created Date: Sunday December 1st 2019
  * Author: DaGai  <binghan2836@163.com>
  * -----
- * Last Modified: Sunday December 1st 2019 6:05:27 pm
+ * Last Modified: Sunday December 1st 2019 8:27:40 am
  * Modified By:   the developer formerly known as DaGai
  * -----
  * MIT License
@@ -41,45 +41,65 @@
 #include "RandomGenerator.h"
 #include "FloodProblem.h"
 
-TEST(FloodProblemTest,TwoDimensinalObjArray)
+TEST(FloodProblemTest, TwoDimensinalObjArray)
 {
     int matrix[4][4];
 
-    for(int i =0; i < 4; ++i)
+    for (int i = 0; i < 4; ++i)
     {
-        for(int j = 0; j < 4; ++j)
+        for (int j = 0; j < 4; ++j)
         {
             matrix[i][j] = j;
         }
     }
 
     auto obj = MakeTwoDimensinalObj(matrix);
-    EXPECT_EQ(obj.GetValue(0,3),3);
-    EXPECT_EQ(obj.GetValue(1,3),3);
+    EXPECT_EQ(obj.GetValue(0, 3), 3);
+    EXPECT_EQ(obj.GetValue(1, 3), 3);
 
-    obj.SetValue(0,3,10);
+    obj.SetValue(0, 3, 10);
     obj.Cells()[1][3] = 20;
 
-    EXPECT_EQ(obj.GetValue(0,3),10);
-    EXPECT_EQ(obj.GetValue(1,3),20);
+    EXPECT_EQ(obj.GetValue(0, 3), 10);
+    EXPECT_EQ(obj.GetValue(1, 3), 20);
 }
 
-TEST(FloodProblemTest,GetAnchor)
+TEST(FloodProblemTest, Init)
 {
-    int matrix[4][4];
+    int matrix[3][3];
 
-    for(int i =0; i < 4; ++i)
+    int data[] = {0, 8, 0, 7, 2, 6, 0, 5, 0};
+
+    int index = 0;
+    for (int i = 0; i < 3; ++i)
     {
-        for(int j = 0; j < 4; ++j)
+        for (int j = 0; j < 3; ++j)
         {
-            matrix[i][j] = j;
+            matrix[i][j] = data[index];
+            ++index;
         }
     }
 
     auto flood = MakeFloodProblemInstance(matrix);
+    flood.InitSource();
+    auto obj = flood.GetAnchorObj(AnchorObj::TOP_LEFT);
+    EXPECT_EQ(obj.first, 0);
+    EXPECT_EQ(obj.second, 0);
 
-    AnchorObj obj;
-    
-    EXPECT_EQ(flood.GetAnchorObj(obj), true);
+    auto rightObj = flood.GetAnchorObj(AnchorObj::BOTTOM_RIGHT);
+
+    EXPECT_EQ(rightObj.first, 2);
+    EXPECT_EQ(rightObj.second, 2);
+
+    for (int i = 0; i < 9; i++)
+    {
+        EXPECT_EQ(flood.GetPrecursor(i), VertexObj::NIL);
+        EXPECT_EQ(flood.GetPathValue(i), VertexObj::NIL);
+        EXPECT_EQ(flood.GetWeight(i), data[i]);
+    }
+
 }
 
+TEST(FloodProblemTest, Sort)
+{
+}
